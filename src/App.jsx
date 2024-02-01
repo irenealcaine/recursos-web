e18ef47fc44aec6b473d821b8664217685cb9adb
Irene Alcaine <irenealcainealvarez@gmail.com>
2024-02-01 21:17:30 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [categories, setCategories] = useState([])
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch(import.meta.env.VITE_CATEGORY_SHEET)
+      .then((response) => response.json())
+      .then((data) => setCategories(data))
+
+    fetch(import.meta.env.VITE_ITEM_SHEET)
+      .then((response) => response.json())
+      .then((data) => setItems(data))
+  }, [])
+  // console.log(categories);
+  // console.log(items);
+
+  const nestedData = categories.map(category => {
+    return {
+      ...category,
+      items: items.filter(item => item.categoryId === category.categoryId)
+    };
+  });
+  // console.log(nestedData)
+
+  return (
+    <>
+      <h1>Recursos</h1>
+      {nestedData.map((category) => (
+        <div key={category.categoryId}>
+          <h2>{category.categoryName}</h2>
+          <ul>
+            {category.items.map((item) => (
+              <li key={item.itemId}><a href={item.itemLink}>{item.itemName}</a></li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </>
+  )
+}
+
+export default App
